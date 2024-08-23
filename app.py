2cd1a0cf6f2d3000c811fb2373bd9a8a70918202
Tanooj Reddy <142309781+tanooj13@users.noreply.github.com>
2024-08-23 17:43:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+
+import numpy as np
+with open("Estate_Model.pkl", 'rb') as file:
+    model = pickle.load(file)
+
+
+def input():
+    bedrooms = st.number_input("bed rooms ")
+    bathrooms = st.number_input("bath rooms ")
+    status = st.number_input("Status ")
+    size = st.number_input("size")
+    location = st.number_input("location")
+    face = st.number_input("face")
+    type = st.number_input("Type")
+    arr = np.array([[bedrooms, bathrooms, status, size, location, face, type]])
+    return arr
+
+st.write(model.predict(input()))
